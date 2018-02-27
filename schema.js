7904c9c1f276ce334b91e5c0fb3a65a1ca5f4079
Mark Vayngrib <mark@tradle.io>
2018-02-27 13:08:23 -0500

    add ApplyForProduct.contextId

--- schema.js
+++ schema.js
@@ -15,5 +15,6 @@ module.exports = require('protocol-buffers')(`
     required string host = 1;
     required bytes provider = 2;
     required string product = 3;
+    optional bytes contextId = 4;
   }
 `)
